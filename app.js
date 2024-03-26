9b308821f9f08456fe6f312b16790de69acb81eb
Parvez Mohammed <122012007+ParvezMohammed@users.noreply.github.com>
2024-03-26 16:09:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,4 +21,30 @@
 // O/P : Hi There, undefined
 
 // Explanation:
-// "name is undefined" as "nothing was passed as the argument" "while calling the function"
+// "name is undefined" as "nothing was passed as the argument" "while calling the function".
+
+
+// Quiz - 3:
+
+// let a = 10, b = 10, c = 10;
+// if (a == b == c) {
+//     console.log("Yes, it is true");
+// } else {
+//     console.log("No, it is false");
+// }
+
+// O/P : No, it is false
+
+// Explanation:
+// As we have 2 == operators, so they are executed from left to right.
+
+// a == b == c
+// ⇒ (a == b) == c
+// ⇒ (10 == 10) == 10
+// ⇒ true == 10
+// ⇒ 1 == 10
+// ⇒ false
+
+// Why is true converted to 1?
+// The rule for == says if one of the operands is boolean, it's converted to number before the comparison.
+// true is converted to its number value, which is 1.
