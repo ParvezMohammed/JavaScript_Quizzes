7c3c0dcf4c0eef8d46e70863f4e98e65f3eaf7df
Parvez Mohammed <122012007+ParvezMohammed@users.noreply.github.com>
2024-03-26 16:03:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Quiz - 1:
 // let x = 3 > 2 > 1;
 
 // console.log(x);
@@ -8,3 +9,16 @@
 // 3 > 2 = true
 // true = 1
 // 1 > 1 = False
+
+
+// Quiz - 2:
+
+// function sayHi(name) {
+//     return `Hi There, ${name}`;
+// }
+// console.log(sayHi());
+
+// O/P : Hi There, undefined
+
+// Explanation:
+// "name is undefined" as "nothing was passed as the argument" "while calling the function"
